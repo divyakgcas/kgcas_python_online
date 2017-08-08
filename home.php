554fc8c08c1bf85720ca08310062a78c3be83bc8
divyakgcas <30406487+divyakgcas@users.noreply.github.com>
2017-08-08 17:01:28 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title> WELCOME </title> 
+<center>
+<h1>ONLINE ACCREDITATION SYSTEM<H1>
+<h2> Welcome to our new accreditation system.
+    After the registration you can easily get a media accreditation for EJU events.<h2>
+</center>
+</head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, / initial-scale=1">
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/3/w3.css">
+<body>
+
+<!-- Content will go here -->
+<nav class="w3-bar w3-black">
+  <a href="#home" class="w3-button w3-bar-item">Home</a>
+  <a href="#band" class="w3-button w3-bar-item">Test</a>
+  <a href="#tour" class="w3-button w3-bar-item">Tour</a>
+  <a href="#contact" class="w3-button w3-bar-item">Contact</a>
+ </nav>
+ <center><img src="divi.gif"height="300"width="300"></center>
+</body>
+</html>
